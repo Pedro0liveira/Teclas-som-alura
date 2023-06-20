d3a7ef639a9718bfffaf446016822860e06ad744
Pedro0liveira <oliveirapedro7373@gmail.com>
2023-06-20 15:02:19 -0300

    final

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ function tocaSom (idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-
 for (let i = 0; i < listaDeTeclas.length; i++) {
 
     const tecla = listaDeTeclas[i];
@@ -14,7 +13,6 @@ for (let i = 0; i < listaDeTeclas.length; i++) {
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
-
     tecla.onkeydown = function (evento) {
         const codeKey = evento.code;
 
